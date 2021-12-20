b6f8a83412bc5832469a1b82dab886c8275c5b5f
CH.Nithin Chakravarthy <94435704+Chakravarthy738@users.noreply.github.com>
2021-12-20 14:25:00 +0530

    makeView

--- battleship.py
+++ battleship.py
@@ -43,6 +43,8 @@ Parameters: dict mapping strs to values ; Tkinter canvas ; Tkinter canvas
 Returns: None
 '''
 def makeView(data, userCanvas, compCanvas):
+    drawGrid(data,userCanvas,data["userboard"],True)
+    drawGrid(data,userCanvas,data["computerboard"],False)
     return
 
 
